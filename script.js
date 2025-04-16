55f0e4213e35bf400eaddba8f335bf0cbd5a3c92
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-04-16 22:20:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let newdiv =  document.createElement('div');
+newdiv.textContent =prompt();
+document.body.appendChild(newdiv);
+
+newdiv.style.color = 'yellow'
+newdiv.style.textAlign = 'center'
+newdiv.style.marginTop = '300px'
+
+
+document.body.style.backgroundColor ='red'    
